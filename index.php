0a0bbe7e7addfb8e5168302554af4b0f72f49fca
LatHalim <hamitovich86@mail.ru>
2020-10-28 22:06:36 +0300

    26-2

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,14 +7,16 @@
 </head>
 
 <body>
-    <form id="with_file_form" action="/ts28.php" method="POST" enctype="multipart/form-data">
-        <input type="text" name="name"><br>
-        <input type="text" name="phone"><br>
-        <input type="file" name="file" id="file_form" accept="image/gif, image/jpeg, image/png"> <br>
-        <button type="submit">Отправить</button>
+   <form action="/ts26.php" method="GET">
+        <div><input type="text" name="name" value="<?php echo $_GET['name'] ??  '' ?>"></div>
+    <button type="submit">Отправить</button>
     </form>
-    <script src="/jquery.js"></script>
-    <script src="/ts28.js"></script>
-</body>
 
+    <form action="/server.php" method="POST">
+        <div><input type="text" name="lastname" value=""></div>
+    <div><input type="text" name="phone" value="" placeholder="+79999999999" ></div>
+    <button type="submit">Отправить</button>
+    </form>
+
+</body>
 </html>
--- server.php
+++ server.php
@@ -1,23 +1,63 @@
 <?php
+
 /**
  * @param массив $_POST
  * @return записывает данные в переменные
  */
-$postArray = $_POST;
- class UserData 
- {
-    public $lastname;
-    public $phone;
 
-    public function checkData($postArray){
-       if(isset($_POST) == ''){
-          $lastname = $_POST['lastname'];
-          $this->lastname = $newLastneme;
-          $phone = $_POST['phone'];
-          $this->phone = $newPhone;
-       }
-    }
- }
- $usData = new UserData();
- $usData->checkData($postArray);
- var_dump($usData);
+function isValid($value)
+{
+  return isset($value) && $value !== '';
+}
+
+class UserData
+{
+  private $lastname;
+  private $phone;
+
+  public function __construct($lastname,  $phone)
+  {
+    $this->lastname = $lastname;
+    $this->phone    = $phone;
+  }
+
+  public function isDataOk()
+  {
+
+    return isValid($this->lastname) && isValid($this->phone);
+  }
+  public function getLastname()
+  {
+    return $this->lastname;
+  }
+  /* public function getPhone()
+  {
+   return $this->phone =  preg_replace('#[^0-9]#', '', $this->phone);
+
+    // $s = '89278564234';
+    // $new_s = preg_replace('/^8/', '7', $s);
+  } */
+  public function getPhone()
+  {
+    $this->phone = trim($this->phone);
+
+    $validPhone = preg_replace(
+      array(
+        '/[\+]?([7|8])[-|\s]?\([-|\s]?(\d{3})[-|\s]?\)[-|\s]?(\d{3})[-|\s]?(\d{2})[-|\s]?(\d{2})/'
+      ),
+      array(
+        '+7$2$3$4$5'
+      ),
+      $this->phone
+    );
+    return $validPhone;
+  }
+}
+
+$userData = new UserData($_POST['lastname'], $_POST['phone']);
+
+if ($userData->isDataOk()) {
+  header('location: /thanks.php?name=' . $userData->getLastname() . $userData->getPhone());
+} else {
+  header('location: /');
+}
